46778847d80ed276f8c23ffc3b3e964dc7a5ab4d
Dmitry Bozhko <dmitrybozhko@mail.ru>
2021-09-06 08:03:58 +0700

    В post.html добавлены динамические ссылки на предыдущий/последующий посты

--- src/posts/models.py
+++ src/posts/models.py
@@ -30,6 +30,8 @@ class Post(models.Model):
     thumbnail = models.ImageField()
     categories = models.ManyToManyField(Category) #сразу мн.число у переменной; у многих постов могут быть много категорий
     featured = models.BooleanField() # те посты, которые будут показаны на домашней странице
+    previous_post = models.ForeignKey('self', related_name='previous', on_delete=models.SET_NULL, blank=True, null=True )
+    next_post = models.ForeignKey('self', related_name='next', on_delete=models.SET_NULL, blank=True, null=True )
 
     def __str__(self):
         return self.title
